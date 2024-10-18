aef5e2ae531e5d0cc9c1badd267f7cc5ccb15e9d
fred-super <ifd1507@gmail.com>
2024-10-18 13:48:13 +0300

    Add files via upload

---
+++ dev_exp_linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+double dev_exp(double *radioactivity, double *time, int M, double decay_time)
+{   
+    double deviation1;
+    double sum_residuals1 = 0;
+
+    for ( int i = 0; i < M; i++ )
+    {
+        sum_residuals1 = sum_residuals1 + (radioactivity[i] - exp(-1 * time[i] / decay_time))
+    }
+    deviation1 = sqrt(sum_residuals1) / M;
+    return deviation1;
+}
+
+double dev_linear(double *radioaktivity, double *time, int M, double decay_rate)
+{
+    double deviation2;
+    double sum_residuals2 = 0;
+
+    for ( int i = 0; i < M; i++ )
+    {
+        sum_residuals2 = sum_residuals2 + (radioactivity[i] - (1 - time[i] / decay_rate))
+    }
+    deviation2 = sqrt(sum_residuals2) / M;
+    return deviation2; 
+}
---
+++ dev_exp_linear.h
@@ -0,0 +1,7 @@
+#ifndef DEFINE
+#define DEFINE
+
+double dev_exp(double *radioactivity, double *time, int M, double decay_time);
+double dev_linear(double *radioaktivity, double *time, int M, double decay_rate);
+
+#endif
